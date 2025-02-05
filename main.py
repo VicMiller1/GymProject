6686fca47a2cbc80d5d3e2183b47c1e277fe3270
LiyaYe1 <yeyufei28@gmail.com>
2025-02-05 09:25:32 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
+MembershipType = []
+members = []
+
+def adddetails():
+  firstname = str(input("enter your fist name:"))
+  lastname = str(input("Enter your last name: "))
+  membership = str(input("enter your membership type:"))
+  duration = int(input("enter your membership duration:"))
+
+  totalprice = 0
+  if membership == "Basic":
+    totalprice = 29.99 * duration
+  elif membership == "Standard":
+    totalprice = 49.99 * duration
+  elif membership == "Premium":
+    totalprice = 69.99 * duration
+
+  MembershipType.append([membership, totalprice])
+  members.append([firstname, lastname, MembershipType, duration])
+
+def savedetails():
+  with open (
+  
+
 
